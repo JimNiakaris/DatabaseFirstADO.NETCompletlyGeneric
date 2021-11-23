870c763744a41159087e3d7d2ca9950f64d935e2
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-11-23 12:38:18 +0200

    Adding a POCO in

--- DatabaseFirstADO/Program.cs
+++ DatabaseFirstADO/Program.cs
@@ -4,40 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using DatabaseFirstADO.entities;
 
 namespace DatabaseFirstADO
 {
-    class Program
+    class Program<T>
     {
         static void Main(string[] args)
         {
-            string connectionString = @"Server=NBD-LW13-N22;Database=MyDatabase;Trusted_Connection=True";
+            string connectionString = @"Server=NBD-LW13-N;Database=MyDatabase;Trusted_Connection=True";
             string sql = "SELECT * FROM dbo.mytable";
-            SqlConnection connection = new SqlConnection(connectionString);
-            using(connection)
+            string sql2 = "SELECT * FROM dbo.mytable WHERE id = 1002";
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
                     connection.Open();
                     Console.WriteLine(connection.State);
-                    SqlCommand command = new SqlCommand(sql,connection);
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"ID: {reader.GetInt32(0)}, my_number: {reader.GetInt32(1)}, " +
-                                                                    $"my_string:{reader.GetString(2)}, " +
-                                                                    $"my_string2: {reader.GetString(3)}");
-                    }
-
-                    connection.Close();
+                    ICollection<T> myTables1 = SelectData(sql, connection);
+                    PrintData(myTables1);
+                    SelectData(sql2, connection);
                 } 
                 catch(SqlException ex)
                 {
                     Console.WriteLine($"Error occured: {ex.Message}");
                 }
-                
+                finally
+                {
+                    connection.Close();
+                }
             }
-            
+        }
+
+        static ICollection<T> SelectData(string sql, SqlConnection connection)
+        {
+            ICollection<MyTable> myTables = new List<MyTable>();
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                MyTable myTable = new MyTable(reader.GetInt32(0), reader.GetInt32(1) , reader.GetString(2), reader.GetString(3));
+                myTables.Add(myTable);
+            }
+            if(!reader.IsClosed)
+            {
+                reader.Close();
+            }
+            return ((ICollection<T>)myTables);
+        }
+
+        static void PrintData(ICollection<T> table)
+        {
+            List<MyTable> myTables = table;
+            Console.WriteLine(table[0]);
         }
     }
 }
---
+++ DatabaseFirstADO/entities/MyTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstADO.entities
+{
+    class MyTable
+    {
+        public int Id { get; set; }
+        public int MyNumber { get; set; }
+        public string MyString { get; set; }
+        public string MyString2 { get; set; }
+
+        public MyTable()
+        {
+
+        }
+
+        public MyTable(int Id, int MyNumber, string MyString, string MyString2)
+        {
+            this.Id = Id;
+            this.MyNumber = MyNumber;
+            this.MyString = MyString;
+            this.MyString2 = MyString2;
+        }
+
+        public override string ToString()
+        {
+            string result = $"ID: {Id}, my_number: {MyNumber}, " +
+                                                            $"my_string:{MyString}, " +
+                                                            $"my_string2: {MyString2}";
+            return(result);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
